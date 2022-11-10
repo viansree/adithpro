cde6bf531f51df3028e5e28a870d53881e149ea1
adithview <aathil444@gmail.com>
2022-11-10 14:33:15 +0530

    second edit by adith

--- src/one/Newclass.java
+++ src/one/Newclass.java
@@ -6,6 +6,7 @@ public class Newclass {
 		// TODO Auto-generated method stub
 		System.out.println("Hiiiiiii adith");
 		System.out.println("iam adith");
+		System.out.println("second edit...");
 
 	}
 
