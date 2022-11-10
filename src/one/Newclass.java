4292839680e5a5fdcf997b4f93bd6cb21543c77a
veenaraj <veenaraj1772@gmail.com>
2022-11-10 14:48:47 +0530

    new edit 3

--- src/one/Newclass.java
+++ src/one/Newclass.java
@@ -4,7 +4,7 @@ public class Newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hiiiiiii adith");
+		System.out.println("Hiiiiiii gio.....");
 		System.out.println("iam adith");
 		System.out.println("second edit...");
 
