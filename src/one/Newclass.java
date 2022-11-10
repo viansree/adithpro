d7567f2f0408f8fa57589bb216deb0eeb89f856b
adithview <aathil444@gmail.com>
2022-11-10 13:15:41 +0530

    changemade

--- src/one/Newclass.java
+++ src/one/Newclass.java
@@ -5,6 +5,7 @@ public class Newclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hiiiiiii adith");
+		System.out.println("iam adith");
 
 	}
 
