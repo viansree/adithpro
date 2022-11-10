00a0a8eb186e02347ab6b8eddd55e15eeb1884c2
veenaraj <veenaraj1772@gmail.com>
2022-11-10 13:04:40 +0530

    new commiting

---
+++ src/one/Newclass.java
@@ -0,0 +1,11 @@
+package one;
+
+public class Newclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hiiiiiii adith");
+
+	}
+
+}
---
+++ src/two/Newtwo.java
@@ -0,0 +1,10 @@
+package two;
+
+public class Newtwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello adith");
+	}
+
+}
